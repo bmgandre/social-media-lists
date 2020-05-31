6d7bbad1d7d185e608764c1ff7b9f455b89611a9
André B. M. Gomes <bmg.andre@gmail.com>
2020-05-31 18:18:39 -0300

    [post] add unit test for search with filters

--- src/Tests.Unit/Application/Posts/Queries/PostQuerySteps.cs
+++ src/Tests.Unit/Application/Posts/Queries/PostQuerySteps.cs
@@ -1,12 +1,16 @@
 ﻿using Moq;
+using SocialMediaLists.Application.Contracts.Common.Data;
 using SocialMediaLists.Application.Contracts.Posts.Models;
 using SocialMediaLists.Application.Contracts.Posts.Repositories;
 using SocialMediaLists.Application.Contracts.Posts.Validators;
 using SocialMediaLists.Application.Contracts.SocialLists.Repositories;
 using SocialMediaLists.Application.Posts.Queries;
+using SocialMediaLists.Domain.People;
 using SocialMediaLists.Domain.Posts;
+using SocialMediaLists.Domain.SocialLists;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
@@ -33,10 +37,7 @@ namespace SocialMediaLists.Tests.Unit.Application.Posts.Queries
                 .ReturnsAsync(new List<Post>());
 
             _readSocialListRepository = new Mock<IReadSocialListsRepository>();
-
             _postFilterValidator = new Mock<IPostSearchRequestValidator>();
-            _postFilterValidator
-                .Setup(x => x.ValidateAndThrowAsync(It.IsAny<PostSearchRequest>(), It.IsAny<CancellationToken>()));
 
             _postQuery = new PostQuery(_readPostRepository.Object, _readSocialListRepository.Object, _postFilterValidator.Object);
         }
@@ -44,12 +45,18 @@ namespace SocialMediaLists.Tests.Unit.Application.Posts.Queries
         [Given(@"I have a request for searching posts")]
         public void given_i_have_a_request_for_searching_posts()
         {
+            _postFilter = new PostSearchRequest();
         }
 
         [Given(@"no data is provided for filtering the posts")]
         public void given_no_data_is_provided_for_filtering_the_posts()
         {
-            _postFilter = new PostSearchRequest();
+        }
+
+        [Given(@"a set of social lists are provided for filtering the posts")]
+        public void GivenASetOfSocialListsAreProvidedForFilteringThePosts()
+        {
+            _postFilter.Lists = new[] { "List 1" };
         }
 
         [When(@"I search the posts")]
@@ -74,7 +81,25 @@ namespace SocialMediaLists.Tests.Unit.Application.Posts.Queries
         [Then(@"the post filter validator should be reached")]
         public void then_the_post_filter_validator_should_be_reached()
         {
-            _postFilterValidator.Verify(mock => mock.ValidateAndThrowAsync(It.IsAny<PostSearchRequest>(), It.IsAny<CancellationToken>()), Times.Once());
+            _postFilterValidator.Verify(mock =>
+                mock.ValidateAndThrowAsync(
+                    It.IsAny<PostSearchRequest>(),
+                    It.IsAny<CancellationToken>()
+                    ), Times.Once());
+        }
+
+        [Then(@"the social lists repository should be reached")]
+        public void then_the_social_lists_repository_should_be_reached()
+        {
+            _readSocialListRepository.Verify(mock =>
+                mock.SearchAndProjectAsync(
+                    It.IsAny<ISpecification<SocialList>>(),
+                    It.IsAny<Expression<Func<SocialList, IEnumerable<SocialListPerson>>>>(),
+                    It.IsAny<Expression<Func<SocialListPerson, Person>>>(),
+                    It.IsAny<Expression<Func<Person, ICollection<SocialAccount>>>>(),
+                    It.IsAny<Expression<Func<SocialList, IEnumerable<SocialAccount>>>>(),
+                    It.IsAny<CancellationToken>()
+                    ), Times.Once());
         }
     }
 }
